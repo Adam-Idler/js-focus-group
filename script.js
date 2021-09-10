e8f5fac11d1e34f220c76c159090d51c3c99dca3
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-10 17:14:50 +0700

    Выполнил второе задание

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'My project';
+let screens= 'Простые, Сложные, Интерактивные';
+let screenPrice = 2500;
+let rollback = 50;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert('Привет!');
-console.log('Привет из консоли!');
+console.log('type of title:', typeof title);
+console.log('type of fullPrice:', typeof fullPrice);
+console.log('type of adaptive:', typeof adaptive);
+
+console.log('Длина строки screens:', screens.length);
+
+console.log(`Стоимость верстки экранов: ${screenPrice} рублей`,
+            `\nСтоимость разработки сайта: ${fullPrice} рублей`);
+
+console.log('Массив screens:', screens.toLowerCase().split(' '));
+
+console.log('Процент отката посреднику за работу:', fullPrice * (rollback / 100));
