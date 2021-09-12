cb2c63d9baf58a38ccc197ff0b2c34e52d3ef38e
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-12 16:17:43 +0700

    Выполнил 3 урок

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let title = 'My project';
-let screens= 'Простые, Сложные, Интерактивные';
-let screenPrice = 2500;
-let rollback = 50;
-let fullPrice = 10000;
-let adaptive = true;
+const title = prompt('Как называется Ваш проект?', 'Калькулятор стоимости сайта');
+const screens= prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Добавление JavaScript');
+let servicePrice1 = +prompt('Сколько это будет стоить?', '5000');
+let service2 = prompt('Какой тип второй дополнительной услуги?', 'Добавление CMS');
+let servicePrice2 = +prompt('Сколько будет стоить вторая услуга?', '7000');
+
+let rollback = 10;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - Math.ceil(fullPrice / 100 * rollback); 
+
+if (fullPrice >= 30000) {
+    console.log('Даем скидку 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log('Даем скидку 5%');
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+    console.log('Скидка не предусмотрена');
+} else {
+    console.log('Что-то пошло не так...');
+}
 
 console.log('type of title:', typeof title);
 console.log('type of fullPrice:', typeof fullPrice);
@@ -16,4 +33,5 @@ console.log(`Стоимость верстки экранов: ${screenPrice} р
 
 console.log('Массив screens:', screens.toLowerCase().split(' '));
 
-console.log('Процент отката посреднику за работу:', fullPrice * (rollback / 100));
+console.log('Итоговую стоимость за вычетом процента отката:', servicePercentPrice);
+console.log('Процент отката посреднику за работу:', Math.ceil(fullPrice / 100 * rollback));
