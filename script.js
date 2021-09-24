d597c09b6899a07b3329409c67a2f8a8a00a03fb
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-24 16:30:21 +0700

    Выполнил 4 урок

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const title = prompt('Как называется Ваш проект?', 'Калькулятор стоимости сайта');
-const screens= prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные');
 const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 const adaptive = confirm('Нужен ли адаптив на сайте?');
 
@@ -9,29 +9,62 @@ let service2 = prompt('Какой тип второй дополнительно
 let servicePrice2 = +prompt('Сколько будет стоить вторая услуга?', '7000');
 
 let rollback = 10;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - Math.ceil(fullPrice / 100 * rollback); 
+let servicePercentPrice; 
+let fullPrice;
 
-if (fullPrice >= 30000) {
-    console.log('Даем скидку 10%');
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-    console.log('Даем скидку 5%');
-} else if (fullPrice < 15000 && fullPrice >= 0) {
-    console.log('Скидка не предусмотрена');
-} else {
-    console.log('Что-то пошло не так...');
+
+
+
+const showTypeOf = function(variable) {
+    console.log(varable, typeof variable);
+};
+
+const getAllServicePrices = (addService1, addService2) => {
+    return +addService1 + addService2;
+};
+
+function getFullPrice(layoutPrice, addPrice) {
+    return +layoutPrice + addPrice;
 }
 
-console.log('type of title:', typeof title);
-console.log('type of fullPrice:', typeof fullPrice);
-console.log('type of adaptive:', typeof adaptive);
+const getTitle = (title) => {
+    return title.trim()[0].toUpperCase() + title.trim().slice(1).toLowerCase();
+};
 
-console.log('Длина строки screens:', screens.length);
+const getServicePercentPrices = (price, rollback) => {
+    return price - Math.ceil(price / 100 * rollback);
+};
+
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        return "Даем скидку 10%";
+    } else if (price >= 15000 && price < 30000) {
+        return "";
+    } else if (price < 15000 && price >= 0) {
+        return "Даем скидку 5%";
+    } else {
+        return "Что-то пошло не так...";
+    }
+};
+
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+
+
+
+console.log(getRollbackMessage(fullPrice));
+
+showTypeOf(type);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
 
 console.log(`Стоимость верстки экранов: ${screenPrice} рублей`,
             `\nСтоимость разработки сайта: ${fullPrice} рублей`);
 
 console.log('Массив screens:', screens.toLowerCase().split(' '));
-
-console.log('Итоговую стоимость за вычетом процента отката:', servicePercentPrice);
+console.log(getRollbackMessage(fullPrice));
+console.log('Итоговую стоимость за вычетом процента отката:', getServicePercentPrices());
 console.log('Процент отката посреднику за работу:', Math.ceil(fullPrice / 100 * rollback));
