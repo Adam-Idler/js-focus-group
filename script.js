21b170bc515c897314073324d713a9ae1184e23f
Pavel Rakov <p.rakov@elfsight.com>
2021-10-24 22:03:03 +0700

    Какой-то коммит

--- script.js
+++ script.js
@@ -9,32 +9,48 @@ let DomElement = function(selector, height, width, bg, fontSize) {
 };
 
 DomElement.prototype.createElem = function() {
-  let elem;
-  if (this.selector[0] === '.') {
+    let elem;
+    if (this.selector[0] === '.') {
 
-    elem = document.createElement('div');
-    elem.classList.add(`${this.selector.slice(1)}`);
-    elem.textContent = 'Заголовок с классом';
+        elem = document.createElement('div');
+        elem.classList.add(`${this.selector.slice(1)}`);
+        elem.textContent = 'Двигайте квадрат стрелками на клавиатуре';
 
-  } else if (this.selector[0] === '#') {
+    } else if (this.selector[0] === '#') {
 
-    elem = document.createElement('p');
-    elem.id = this.selector.slice(1);
-    elem.textContent = 'Параграф с id';
-  }
+        elem = document.createElement('p');
+        elem.id = this.selector.slice(1);
+        elem.textContent = 'Параграф с id';
+    }
 
-  elem.style.cssText = `
-    height: ${this.height};
-    width: ${this.width};
-    background: ${this.bg};
-    font-size: ${this.fontSize};
-  `;
+    elem.style.cssText = `
+        height: ${this.height};
+        width: ${this.width};
+        background: ${this.bg};
+        font-size: ${this.fontSize};
+        text-align: center;
+        position: absolute;
+        top: 200px;
+        left: 200px;
+    `;
 
-  document.body.append(elem);
+    document.body.append(elem);
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === 'ArrowUp') {
+            elem.style.top = elem.style.top.slice(0, -2) - 10 + 'px';
+        } else if (event.key === 'ArrowLeft') {
+            elem.style.left = elem.style.left.slice(0, -2) - 10 + 'px';
+        } else if (event.key === 'ArrowRight') {
+            elem.style.left = +elem.style.left.slice(0, -2) + 10 + 'px';
+        } else if (event.key === 'ArrowDown') {
+            elem.style.top = +elem.style.top.slice(0, -2) + 10 + 'px';
+        }
+    });
 };
 
-let domElementDiv = new DomElement('.container', 400, 500, 'green', 24);
-domElementDiv.createElem();
+let square = new DomElement('.square', 100, 100, '#5F9EA0', 'absolute');
 
-let domElementP = new DomElement('#paragraph', 30, 500, 'red', 16);
-domElementP.createElem();
+document.addEventListener("DOMContentLoaded", function() {
+    square.createElem();
+});
